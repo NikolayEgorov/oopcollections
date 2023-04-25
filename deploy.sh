195cd71f26e10fab1ffc8d934c7e028faa3884f3
Mykola Yehorov <egorov.n@vuso.ua>
2023-04-25 11:47:52 +0300

    change start domain

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,11 @@ ssh-add ~/.ssh/id_rsa
 echo 'updating project...'
 
 PROJECTS_PATH="/home/mykola/dotNetProjects"
+PROJECTS_NAME="collections"
 
 cd $PROJECTS_PATH
 
-ssh -F ~/.ssh/config vm rm -rf $PROJECTS_PATH/labrob1/*
-scp -F ~/.ssh/config -r labrob1/* vm:$PROJECTS_PATH/labrob1/
+ssh -F ~/.ssh/config vm rm -rf $PROJECTS_PATH/$PROJECTS_NAME/*
+scp -F ~/.ssh/config -r $PROJECTS_NAME/* vm:$PROJECTS_PATH/$PROJECTS_NAME/
 
 ssh-agent -k
